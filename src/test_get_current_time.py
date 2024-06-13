f2448175ff059a49315d1c01de96aac36e332656
jay-snyder <github@catch.fail>
2024-06-12 22:47:14 -0700

    fix false negative

--- src/test_get_current_time.py
+++ src/test_get_current_time.py
@@ -3,10 +3,13 @@ from unittest.mock import patch, Mock
 
 
 @patch(
-    "src.get_current_time.get_current_timestamp",
+    "get_current_time.get_current_timestamp",
     Mock(return_value="2024-06-12T20:04:10.885479"),
 )
 def test_handler():
     response = handler(None, None)
 
     assert response == '{"currentTime": "2024-06-12T20:04:10.885479"}'
+
+if __name__ == "__main__":
+    test_handler()
